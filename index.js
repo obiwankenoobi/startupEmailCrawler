9b139f06f2ec4b2e6ec47c177f997bce8fb53182
obivankanoobi <artium1@gmail.com>
2018-11-10 10:57:00 +0200

    refactoring

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
+const sniper = require("./lib/sniper");
+const regular = require("./lib/regular");
 
-
-const sniper = require("./lib/sniper")
-const regular = require("./lib/regular")
-
-if (process.argv[2] == "--sniper") {
-    sniper()
+switch (process.argv[2]) {
+  case "--sniper":
+    sniper();
+    break;
+  case "--reg":
+    regular();
+    break;
+  default:
+    console.log("please choose mode (--reg or --sniper)");
 }
-else if (process.argv[2] == "--reg") {
-    regular()
-} 
-else {
-    console.log("please choose mode (--reg or --sniper)")
-}
-
-
